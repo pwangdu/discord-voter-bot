360e8262b631ff4b2d5634a038e6892811f1c302
medlee <pankajwahane@gmail.com>
2018-03-30 22:32:51 +0530

    Adding specific channel

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ bot.on("message", async message => {
     if (message.author.bot) return;
     if (message.channel.type === "dm") return;
     if (!message.content.startsWith(prefix)) return;
-
+    if(message.channel.name !== '${botsettings.allowed-channel}') return;
     let messageArray = message.content.split(" ");
     let command = messageArray[0];
     let args = messageArray.slice(1);
